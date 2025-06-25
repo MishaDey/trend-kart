b0fa75e0a404fa2238547a99d95116f6df41172a
= <=>
2025-06-25 11:02:51 +0530

    Added Initial Project Setup

---
+++ src/main/java/com/trend_kart/TrendkartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trend_kart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendkartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendkartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trend_kart/TrendkartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trend_kart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendkartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
